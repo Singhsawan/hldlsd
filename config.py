8906cbf12a5b3631123527b57fe6ca11d66cdbcc
Nothing bro ✓ <125857863+Singhsawan@users.noreply.github.com>
2023-07-04 18:09:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,28 +1,28 @@
 import os
 
-API_ID = os.environ.get('API_ID', '') 
-API_HASH = os.environ.get('API_HASH', '') 
-BOT_TOKEN = os.environ.get('BOT_TOKEN', '') 
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+API_ID = os.environ.get('API_ID', '29362464') 
+API_HASH = os.environ.get('API_HASH', '31973315b0872a0478886de31a1e4848') 
+BOT_TOKEN = os.environ.get('BOT_TOKEN', '6329743533:AAEIOlPy0emRd3zzBUE9LrEPChD4kLSj2CQ') 
+OWNER_ID = int(os.environ.get("OWNER_ID", "5651594253"))
 
-ADMINS = list(int(i) for i in os.environ.get("ADMINS", "").split(" ")) if os.environ.get("ADMINS") else []
+ADMINS = list(int(i) for i in os.environ.get("ADMINS", "5651594253").split(" ")) if os.environ.get("ADMINS") else []
  
 if OWNER_ID not in ADMINS:
     ADMINS.append(OWNER_ID)
 
-MONGODB = os.environ.get('MONGODB', '')
+MONGODB = os.environ.get('MONGODB', 'mongodb+srv://Rishikesh001:Rishikesh001@cluster0.lqncnak.mongodb.net/?retryWrites=true&w=majority')
 DATABASE_NAME = os.environ.get('DATABASE_NAME', 'linkfindbot') 
 COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'nvslinkbot')
-UPDATE_CHANNEL =  os.environ.get('UPDATE_CHANNEL', 'tgnvs')
+UPDATE_CHANNEL =  os.environ.get('UPDATE_CHANNEL', 'one_file')
 USERNAME = UPDATE_CHANNEL
 RESULTS_COUNT = int(os.environ.get('RESULT_COUNTS', 3))
 AUTO_DELETE = os.environ.get('AUTO_DELETE', 'True')
 AUTO_DELETE_TIME = int(os.environ.get('AUTO_DELETE_TIME', 300))
-START_MSG = os.environ.get('START_MSG', '<b>Hey!,\nI am Link Search Bot.\n🤖I Can Search 🔍 What You Want❗\nMade With ❤ By @tgnvs</b>')
-GROUP = os.environ.get('GROUP', 'nvshdmovielink')
+START_MSG = os.environ.get('START_MSG', '<b>Hey!,\nI am Link Search Bot.\n🤖I Can Search 🔍 What You Want❗\nMade With ❤ By @J_shree_ram</b>')
+GROUP = os.environ.get('GROUP', 'Filmy_Fundas')
 FILEBOT = os.environ.get('FILEBOT', 'None')
-BOT_USERNAME = os.environ.get('BOT_USERNAME', 'nvslinkbot')
-OWNER_USERNAME = os.environ.get('OWNER_USERNAME', 'tgnvs')
+BOT_USERNAME = os.environ.get('BOT_USERNAME', 'Search_your_mov_bot')
+OWNER_USERNAME = os.environ.get('OWNER_USERNAME', 'J_shree_ram')
 HOWTO = os.environ.get('HOWTO', 'https://t.me/tgnvs/8')
 START_PIC = os.environ.get('START_PIC', 'https://graph.org/file/ff2999e57bf1ae1f99e7e.jpg')
 
