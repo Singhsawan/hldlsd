4f3557f1df05430ca7011d832ee5a10eca1f1eaa
Jack <125857863+Singhsawan@users.noreply.github.com>
2023-10-15 09:03:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 API_ID = os.environ.get('API_ID', '29362464') 
 API_HASH = os.environ.get('API_HASH', '31973315b0872a0478886de31a1e4848') 
-BOT_TOKEN = os.environ.get('BOT_TOKEN', '6329743533:AAEIOlPy0emRd3zzBUE9LrEPChD4kLSj2CQ') 
+BOT_TOKEN = os.environ.get('BOT_TOKEN', '6353560897:AAE_ySQ4StMj2jCWWxPvzLmvIagouqiFjE0') 
 OWNER_ID = int(os.environ.get("OWNER_ID", "5651594253"))
 
 ADMINS = list(int(i) for i in os.environ.get("ADMINS", "5651594253").split(" ")) if os.environ.get("ADMINS") else []
@@ -10,10 +10,10 @@ ADMINS = list(int(i) for i in os.environ.get("ADMINS", "5651594253").split(" "))
 if OWNER_ID not in ADMINS:
     ADMINS.append(OWNER_ID)
 
-MONGODB = os.environ.get('MONGODB', 'mongodb+srv://Rishikesh001:Rishikesh001@cluster0.lqncnak.mongodb.net/?retryWrites=true&w=majority')
+MONGODB = os.environ.get('MONGODB', 'mongodb+srv://gareenaff3:lauda@cluster0.ycdzch5.mongodb.net/?retryWrites=true&w=majority')
 DATABASE_NAME = os.environ.get('DATABASE_NAME', 'linkfindbot') 
 COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'nvslinkbot')
-UPDATE_CHANNEL =  os.environ.get('UPDATE_CHANNEL', 'one_file')
+UPDATE_CHANNEL =  os.environ.get('UPDATE_CHANNEL', 'bot_channel_011')
 USERNAME = UPDATE_CHANNEL
 RESULTS_COUNT = int(os.environ.get('RESULT_COUNTS', 3))
 AUTO_DELETE = os.environ.get('AUTO_DELETE', 'True')
@@ -21,7 +21,7 @@ AUTO_DELETE_TIME = int(os.environ.get('AUTO_DELETE_TIME', 300))
 START_MSG = os.environ.get('START_MSG', '<b>Hey!,\nI am Link Search Bot.\n🤖I Can Search 🔍 What You Want❗\nMade With ❤ By @J_shree_ram</b>')
 GROUP = os.environ.get('GROUP', 'Filmy_Fundas')
 FILEBOT = os.environ.get('FILEBOT', 'None')
-BOT_USERNAME = os.environ.get('BOT_USERNAME', 'Search_your_mov_bot')
+BOT_USERNAME = os.environ.get('BOT_USERNAME', 'Hsiwjeh_bot')
 OWNER_USERNAME = os.environ.get('OWNER_USERNAME', 'J_shree_ram')
 HOWTO = os.environ.get('HOWTO', 'https://t.me/tgnvs/8')
 START_PIC = os.environ.get('START_PIC', 'https://graph.org/file/ff2999e57bf1ae1f99e7e.jpg')
